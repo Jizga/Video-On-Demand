bd51f3f5dfef08013bd24c4b9c5e5a636685f0d3
Vivi <jizga93@gmail.com>
2022-03-30 15:54:32 +0200

    Calle to the API

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 export default function Carousel() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let response = await fetch("https://test-data-interviews.s3.eu-west-1.amazonaws.com/accedoTest.json");
+        response = await response.json();
+        setData(response)
+  
+      } catch (e) {
+        console.error(`Error from database -- ${e}`);
+      }
+    }
+
+    getData()
+  }, [])
+
   return (
     <div>Carousel
       < Card />

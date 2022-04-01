05280ddcfd90946222ef48b69b059b01f1557556
Vivi <jizga93@gmail.com>
2022-04-01 14:50:58 +0200

    History view done

--- src/views/History/History.js
+++ src/views/History/History.js
@@ -1,10 +1,17 @@
 import React, { useContext } from "react";
 import { Context } from "../../context/context";
 import Carousel from "../../components/Carousel/Carousel";
+import styles from "./History.model.css";
 
 export default function History() {
   const { watchedList } = useContext(Context);
-  return <div>
-    <Carousel />
-  </div>;
+  return (
+    <div>
+      {watchedList.length ? (
+        <Carousel data={watchedList} />
+      ) : (
+        <p className={styles.textDefault}>You did not watch any videos yet</p>
+      )}
+    </div>
+  );
 }

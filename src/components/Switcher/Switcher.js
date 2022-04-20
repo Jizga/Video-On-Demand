c36336d0f31ea1de9f8e88f43ad96c9571e248e2
Vivi <jizga93@gmail.com>
2022-04-20 18:58:51 +0200

    switcher button' style done

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,12 +1,19 @@
 import React from "react";
+// import { useAppContext } from "../../Context/Context";
 import styles from "./Switcher.module.scss";
 
 export default function Switcher() {
+  // const { isLigthTheme, setIsLigthTheme } = useAppContext();
+
   return (
-    <div class={styles.switchContainer}>
-      <input type="checkbox" id="toggle" class={styles["toggle--checkbox"]} />
-      <label for="toggle" class={styles["toggle--label"]}>
-        <span class={styles["toggle--label-background"]}></span>
+    <div className={styles.switchContainer}>
+      <input
+        type="checkbox"
+        id="toggle"
+        className={styles["toggle--checkbox"]}
+      />
+      <label htmlFor="toggle" className={styles["toggle--label"]}>
+        <span className={styles["toggle--label-background"]}></span>
       </label>
     </div>
   );
